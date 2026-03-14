6f1e076425a2e6312d602304647cb2110678c7e6
Vlad D. <vladdidyk1985@gmail.com>
2026-03-14 17:52:35 +0200

    feat: add IncreaseAccessLevel method

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -3,7 +3,12 @@ public class User
 {
     private readonly int Id;
     private string Name;
-    private int accessLebel;
+    private int AccessLevel = 0;
 
-    private static int users = 0;
+    private static int Users = 0;
+
+    public void IncreaseAccessLevel()
+    {
+        AccessLevel++;
+    }
 };
